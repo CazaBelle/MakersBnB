376543206cddb6563eb578edd422682f38e4798e
James Hotblack <james.c.hotblack@gmail.com>
2019-01-10 12:17:01 +0000

    Added passing unit test existing user authentification.

--- lib/User.rb
+++ lib/User.rb
@@ -7,5 +7,18 @@ include DataMapper::Resource
   property :name, String 
   property :email, String 
   property :password, BCryptHash
+
+  def self.authenticate(email, password)
+    user = first(email: email)
+    return nil unless user
+    #binding.pry
+    if user.password == password
+      user
+    else
+      "wrong Password"
+    end  
+    
+
+  end
   
 end 
--- spec/units/user_spec.rb
+++ spec/units/user_spec.rb
@@ -1,10 +1,11 @@
 require './lib/User.rb'
+require 'pry'
 
 RSpec.describe User do
     #let(:password) { double :password }
-    let(:user) { User.create(name: "Test", email: "Test@123", password: "1234") }
+    let!(:user) { User.create(name: "Test", email: "Test@123", password: "1234") }
 
-    context 'Sing In' do
+    context 'Sign In' do
         it 'checks name exists' do
             expect(user.name).to eq "Test"
         end
@@ -17,4 +18,14 @@ RSpec.describe User do
             expect(user.password).to eq "1234"
         end
     end
+
+    describe '#authenticate' do
+        user = User.create(name: "Test2", email: "Test2@123", password: "5678")
+
+        it 'exists?' do
+            expect(User.authenticate(user.email, "5678")).to eq user
+        end
+    end
+
+
 end
